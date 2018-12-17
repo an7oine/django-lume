49c365e02d401f3bf64dc0953be8ff4cf0375001
Antti Hautaniemi <antti.hautaniemi@pispalanit.fi>
2018-12-17 10:56:10 +0200

    Poimitaan versionumero git-leimojen mukaan

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+from versiointi import asennustiedot
 
 setup(
   name='django-lume',
-  version='1.0.0',
   description='Django-tuki lume- eli näennäiskentille',
   url='https://git.pispalanit.fi/pit/django-lume',
   author='Antti Hautaniemi',
   author_email='antti.hautaniemi@pispalanit.fi',
   packages=find_packages(),
   include_package_data=True,
-  install_requires=[
-    'Django>=2.0',
-  ],
-  zip_safe=False
+  zip_safe=False,
+  **asennustiedot(__file__)
 )
