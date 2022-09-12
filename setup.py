8f3a576e9ee0b2ea6663a2dabfca83dddf58ac76
Antti Hautaniemi <antti.hautaniemi@pispalanit.fi>
2022-09-12 08:34:25 +0300

    Päivitetty asennusmääritys

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools._install_setup_requires({'setup_requires': ['git-versiointi']})
-from versiointi import asennustiedot
-
-setuptools.setup(
+setup(
+  setup_requires='git-versiointi',
   name='django-lume',
   description='Django-tuki lume- eli näennäiskentille',
   url='https://git.pispalanit.fi/pit/django-lume',
   author='Antti Hautaniemi',
   author_email='antti.hautaniemi@pispalanit.fi',
-  packages=setuptools.find_packages(),
+  packages=find_packages(),
   include_package_data=True,
   zip_safe=False,
-  **asennustiedot(__file__)
 )
